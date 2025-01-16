002203852d427364053f6a3bf781c419acb9230d
hyperbora <hyperbora@gmail.com>
2025-01-16 22:14:04 +0900

    fix: android, iOS flavor 설정 수정

--- android/app/src/main/kotlin/io/hyperbora/coupon_place/MainActivity.kt
+++ android/app/src/main/kotlin/io/hyperbora/coupon_place/MainActivity.kt
@@ -1,5 +1,20 @@
 package io.hyperbora.coupon_place
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "flavor"
+        ).setMethodCallHandler { call, result ->
+            result.success(BuildConfig.FLAVOR)
+        }
+    }
+}
