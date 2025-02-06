234ae4a1f9ac4a94692fdae118eafa7901da427e
hyperbora <hyperbora@gmail.com>
2025-02-06 23:16:12 +0900

    feat: 테블릿, 스마트폰 사이즈에 따라 다른 레이아웃 보여주도록 수정

--- lib/pages/navigation_page.dart
+++ lib/pages/navigation_page.dart
@@ -11,11 +11,97 @@ class NavigationPage extends StatefulWidget {
 
 class _NavigationPageState extends State<NavigationPage> {
   int _selectedIndex = 0;
+  final int _tabletWidth = 600;
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    FolderDashboardPage(
+      title: 'My Coupons',
+    ),
+    Center(child: Text('Add Coupon')),
+    Center(child: Text('Expiring Soon')),
+    Center(child: Text('Settings')),
+  ];
+
+  bool _isTablet(BoxConstraints constraints) {
+    return constraints.maxWidth >= _tabletWidth;
+  }
+
+  Widget tabletLayout(
+    List<String> titles,
+  ) {
+    return Scaffold(
+      body: Row(
+        children: [
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: _onItemTapped,
+            labelType: NavigationRailLabelType.selected,
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.local_offer),
+                label: Text(titles[0]),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.add),
+                label: Text(titles[1]),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.access_time),
+                label: Text(titles[2]),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings),
+                label: Text(titles[3]),
+              ),
+            ],
+          ),
+          VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget smartPhoneLayout(
+    List<String> titles,
+  ) {
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.teal,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_offer),
+            label: titles[0],
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: titles[1],
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: titles[2],
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: titles[3],
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 
   @override
@@ -28,41 +114,12 @@ class _NavigationPageState extends State<NavigationPage> {
       localizations.settings,
     ];
 
-    final List<Widget> widgetOptions = <Widget>[
-      FolderDashboardPage(
-        title: titles[0],
-      ),
-      Center(child: Text('Add Coupon')),
-      Center(child: Text('Expiring Soon')),
-      Center(child: Text('Settings')),
-    ];
-
-    return Scaffold(
-      body: widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_offer),
-            label: localizations.myCoupons,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: localizations.addCoupon,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.access_time),
-            label: localizations.expiringSoon,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: localizations.settings,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      if (_isTablet(constraints)) {
+        return tabletLayout(titles);
+      } else {
+        return smartPhoneLayout(titles);
+      }
+    });
   }
 }
